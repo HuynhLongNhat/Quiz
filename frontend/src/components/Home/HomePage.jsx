ea3beed17d904d1842fbb2122367aee052cd9394
Long Nhat <nhathuynh227@gmail.com>
2024-10-19 21:50:42 +0700

    Dessgin homepage

--- frontend/src/components/Home/HomePage.jsx
+++ frontend/src/components/Home/HomePage.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
-
+import videoHomePage from "../../assets/video-homepage.mp4";
 const HomePage = () => {
   return (
-    <div>HomePage</div>
-  )
-}
+    <div className="homepage-container">
+      <video autoPlay loop muted>
+        <source src={videoHomePage} type="video/mp4"></source>
+      </video>
+      <div className="homepage-content">
+        <div className="title-1">
+          Get to know your customers with forms worth filling out
+        </div>
+        <div className="title-2">
+          Collect all the data you need to <b>understand customers</b> with
+          forms designed to be refreshingly different.
+        </div>
+        <div className="title-3">
+          <button>Get started-it's free</button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default HomePage
+export default HomePage;
