4d33ee9bf9392f16c95ba967ef1a5e021581db1d
Long Nhat <nhathuynh227@gmail.com>
2024-10-19 17:01:02 +0700

    index routes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,12 +6,14 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import User from "./components/Users/User.jsx";
 import Admin from "./components/Admin/Admin.jsx";
+import HomePage from "./components/Home/HomePage.jsx";
 
 createRoot(document.getElementById("root")).render(
   // <StrictMode>
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />}>
+      <Route index element={<HomePage />} />
         <Route path="/users" element={<User />} />
         <Route path="/admins" element={<Admin />} />
       </Route>
