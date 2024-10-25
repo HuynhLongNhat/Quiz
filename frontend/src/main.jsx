ea84c939cf9cfb7822ed8f708aad837a07dadd7a
Long Nhat <nhathuynh227@gmail.com>
2024-10-25 16:54:31 +0700

    Cấu hình và sử dụng redux persist

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,16 +1,19 @@
 // import { StrictMode } from 'react'
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
-import store from "./store/store";
+import store, { persistor } from "./store/store";
 import { BrowserRouter } from "react-router-dom";
 import Layout from "./Layout";
 import "nprogress/nprogress.css";
+import { PersistGate } from "redux-persist/integration/react";
 createRoot(document.getElementById("root")).render(
   // <StrictMode>
   <Provider store={store}>
-    <BrowserRouter>
-      <Layout />
-    </BrowserRouter>
+    <PersistGate loading={null} persistor={persistor}>
+      <BrowserRouter>
+        <Layout />
+      </BrowserRouter>
+    </PersistGate>
   </Provider>
   // </StrictMode>,
 );
--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,11 +1,35 @@
 // src/store/store.js
-import { configureStore } from '@reduxjs/toolkit';
-import userReducer from './slices/userSlice'; // Import slice
+import { configureStore } from "@reduxjs/toolkit";
+import userReducer from "./slices/userSlice"; // Import slice
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const persistConfig = {
+  key: "root",
+  storage,
+  // whitelist: ["user"], // Chỉ lưu trữ userReducer
+};
+const persistedReducer = persistReducer(persistConfig, userReducer);
 
 const store = configureStore({
   reducer: {
-    user: userReducer,
+    user: persistedReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
-
+export const persistor = persistStore(store);
 export default store;
